803a896b0549d3cfc5fe9744e3a230fd997d6e86
keisuke111 <kesk.k99@gmail.com>
2019-07-05 10:29:20 +0900

    Initial Commit

---
+++ SearchRepository/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SearchRepository
+//
+//  Created by 北邑圭佑 on 2019/07/05.
+//  Copyright © 2019 北邑圭佑. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
