ed0f068e640b45062144d0c8eb3e7218788c839d
keisuke111 <kesk.k99@gmail.com>
2019-07-10 11:31:38 +0900

    fix

--- SearchRepository/TableViewController.swift
+++ SearchRepository/TableViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 import Alamofire
 import SwiftyJSON
 
-class TableViewController: UITableViewController {
+class TableViewController: UITableViewController, UISearchBarDelegate {
     
     @IBOutlet var repositoryTableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    // 遷移先のvcに渡す変数
+    var giveData: String = ""
+    
+    // query(language)
+    var query: String = ""
     
     let semaphore = DispatchSemaphore(value: 0)
     
@@ -21,17 +28,49 @@ class TableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        searchBar.delegate = self
+        
         DispatchQueue.global().async {
             self.getRepository()
             DispatchQueue.main.async {
                 self.repositoryTableView.reloadData()
             }
-            print(self.repositories[0].stars)
         }
         
         repositoryTableView.delegate = self
         repositoryTableView.dataSource = self
         
+    }
+    
+    // MARK: - Search Bar Delegate
+    
+    // cancel button tap
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        // debug
+        print("cancel")
+        
+        searchBar.text = ""
+        self.view.endEditing(true)
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        // debug
+        print("search")
+        
+        // 空検索を弾く
+        if searchBar.text == "" {
+            return
+        }
+        
+        self.query = searchBar.text!
+        
+        DispatchQueue.global().async {
+            self.getRepository(language: self.query)
+            DispatchQueue.main.async {
+                self.repositoryTableView.reloadData()
+            }
+        }
+        self.view.endEditing(true)
     }
 
     // MARK: - Table view data source
@@ -40,10 +79,12 @@ class TableViewController: UITableViewController {
         return 1
     }
 
+    // セルの数を指定
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return repositories.count
     }
 
+    // 各セルの要素を指定
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell", for: indexPath) as? TableViewCell  else {
@@ -54,9 +95,18 @@ class TableViewController: UITableViewController {
         
         cell.fullNameLabel.text = repository.fullName
         cell.descriptionLabel.text = repository.description
-        cell.starsLabel.text = (repository.stars).description
+        cell.starsLabel.text = "☆ " + (repository.stars).description
         
         return cell
+    }
+    
+    // セルが押された時
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        giveData = repositories[indexPath.item].htmlUrl
+        
+        // 遷移
+        performSegue(withIdentifier: "Segue", sender: nil)
     }
 
     /*
@@ -94,43 +144,65 @@ class TableViewController: UITableViewController {
     }
     */
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "Segue" {
+            let vc = segue.destination as! ViewController
+            
+            // debug
+            print(giveData)
+            
+            vc.data = giveData
+        }
     }
-    */
     
-    func getRepository() {
+    // MARK - Function
+    
+    func getRepository(language: String = "swift") {
         
-        let requestUrl = "https://api.github.com/search/repositories?q=language:swift&sort=stars&order=desc"
+        // debug
+        print(language)
         
-        Alamofire.request(requestUrl, method: .get)
+        let requestUrl = "https://api.github.com/search/repositories"
+        let params = [
+            "q": "language:" + language,
+            "sort": "stars",
+            "order": "desc"
+        ]
+        
+        self.repositories.removeAll()
+        
+        Alamofire.request(requestUrl, method: .get, parameters: params)
             .responseJSON { response in
                 guard let object = response.result.value else {
                     return
                 }
                 
+                // debug
+                print(JSON(object)["items"])
+                
                 let json = JSON(object)["items"]
                 
                 json.forEach { (_, json) in
                     let fullName: String = json["full_name"].string!
-                    let description: String = json["description"].string!
+
+                    var description = json["description"].string
+                    if description == nil {
+                        description = " "
+                    }
                     let stars: Int = json["stargazers_count"].int!
                     let htmlUrl: String = json["html_url"].string!
                     
                     // 配列に格納
-                    self.repositories.append(Repository(fullName, description, stars, htmlUrl))
+                    self.repositories.append(Repository(fullName, description!, stars, htmlUrl))
                 }
                 
                 self.semaphore.signal()
         }
         
         semaphore.wait()
-        
     }
 
 }
--- SearchRepository/ViewController.swift
+++ SearchRepository/ViewController.swift
@@ -2,19 +2,43 @@
 //  ViewController.swift
 //  SearchRepository
 //
-//  Created by 北邑圭佑 on 2019/07/05.
+//  Created by 北邑圭佑 on 2019/07/09.
 //  Copyright © 2019 北邑圭佑. All rights reserved.
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKUIDelegate {
 
+    var webView: WKWebView!
+    
+    var data: String = ""
+    
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        view = webView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        
+        let url = URL(string: data)
+        let request = URLRequest(url: url!)
+        webView.load(request)
     }
 
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
